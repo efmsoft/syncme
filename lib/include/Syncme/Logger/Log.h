23585accb185c6fe5032b4654e748e6f49d42f4f
Eduard Mishkurov <emishkurov@gmail.com>
2023-02-08 16:34:40 +0300

    do not redefine LogI...LogE

--- lib/include/Syncme/Logger/Log.h
+++ lib/include/Syncme/Logger/Log.h
@@ -3,18 +3,40 @@
 #ifdef USE_LOGME
   #include <Logme/Logme.h>
 
-  #define LogD LogmeD
-  #define LogI LogmeI
-  #define LogW LogmeW
-  #define LogE LogmeE
+  #ifndef LogD
+    #define LogD LogmeD
+  #endif
+
+  #ifndef LogI
+    #define LogI LogmeI
+  #endif
+
+  #ifndef LogW
+    #define LogW LogmeW
+  #endif
+
+  #ifndef LogE
+    #define LogE LogmeE
+  #endif
 
   #define LogosE(format) \
       {LogmeE(format ". Error: %s", OSERR2);}
 #else
-  #define LogD(...)
-  #define LogI(...)
-  #define LogW(...)
-  #define LogE(...)
+  #ifndef LogD
+    #define LogD(...)
+  #endif
+
+  #ifndef LogI
+    #define LogI(...)
+  #endif
+
+  #ifndef LogW
+    #define LogW(...)
+  #endif
+
+  #ifndef LogE
+    #define LogE(...)
+  #endif
 
   #define LogosE(format)
 #endif
