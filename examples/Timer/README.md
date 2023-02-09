673d4b28f5acf17a6cec7d4702d416b640edeff3
Eduard Mishkurov <emishkurov@gmail.com>
2023-02-09 13:03:08 +0300

    Timer example: the list of demonstrated features has been expanded

--- examples/Timer/README.md
+++ examples/Timer/README.md
@@ -1,3 +1,9 @@
 # Timer example
 
-This example demonstrates the use of watable timers and functions to wait for signaled state of objects (**WaitForMultipleObjects**). Three objects are created: a timer that goes into the signaled state after 3 seconds, a timer that goes into the signaled state after 7 seconds, and an event that is already initially in the signaled state. The **WaitForMultipleObjects** function waits for the signaled state of all objects (because the second parameter **waitAll**=true). Thus, the wait ends after 7 seconds. The example also shows the use of the **TimePoint** object
+This example demonstrates the use of watable timers and functions to wait for a signalled state of objects (**WaitForMultipleObjects**).
+
+Three objects are created: a timer that goes into the signaled state after 3 seconds, a timer that goes into the signaled state after 7 seconds, and an event that is already initially in the signaled state. The **WaitForMultipleObjects** function waits for the signaled state of all objects (because the second parameter **waitAll**=true). Thus, the wait ends after 7 seconds. The example also shows the use of the **TimePoint** object
+
+Since the second timer is an autoreset timer it goes to a non-signalled state after completion of **WaitForMultipleObjects**. The first timer remains in the signaled state.
+
+On the next stage the example creates a periodic timer. First time it goes to a signalled state after 1 sec, then each 1/2 sec. 
--- examples/Timer/Timer.cpp
+++ examples/Timer/Timer.cpp
@@ -8,7 +8,7 @@ using namespace Syncme;
 int main()
 {
   auto timer1 = CreateManualResetTimer();
-  auto timer2 = CreateManualResetTimer();
+  auto timer2 = CreateAutoResetTimer();
   auto event3 = CreateNotificationEvent(STATE::SIGNALLED);
 
   TimePoint t0;
@@ -18,6 +18,27 @@ int main()
   EventArray ev(timer1, timer2, event3);
   auto rc = WaitForMultipleObjects(ev, true);
   printf("all objects are signalled after %llu ms\n", t0.ElapsedSince());
+
+  printf("timer1 is %s\n", GetEventState(timer1) == STATE::SIGNALLED ? "signalled" : "not signalled");
+  printf("timer2 is %s\n", GetEventState(timer2) == STATE::SIGNALLED ? "signalled" : "not signalled");
+
+  // timer3 will be signalled first time after 1 sec, then once per 1/2 sec till 
+  // CancelWaitableTimer call
+  auto timer3 = CreateAutoResetTimer();
+
+  TimePoint t1;
+  SetWaitableTimer(timer3, 1000, 500, [t1](HEvent h) { printf("timer3 signalled after %llu ms\n", t1.ElapsedSince()); });
+  for (int i = 0; i < 4; i++)
+    WaitForSingleObject(timer3);
+
+  // The state of object is non-signalled because it is auto reset timer.
+  // It means that WaitForSingleObject/WaitForMultipleObjects resets state to
+  // non-signalled if wait was completed due to state transition of the timer
+  CancelWaitableTimer(timer3);
+
+  // The timer will never signalled any more becase it was cancelled
+  bool f = WaitForSingleObject(timer3, 2000) == WAIT_RESULT::TIMEOUT;
+  printf("timer3 is %s\n", f ? "not signalled because it was cancelled" : "signalled. Library error!!!");
 
   return 0;
 }
